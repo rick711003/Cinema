a27b61f0b4fc29f674727bbac8f82bdaccba49dc
Chen, Rick (Agoda) <rick.chen@agoda.com>
2019-03-19 22:48:34 +0700

    Initial Commit

---
+++ Cinema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by Chen, Rick (Agoda) on 19/3/2562 BE.
+//  Copyright © 2562 Chen, Rick (Agoda). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

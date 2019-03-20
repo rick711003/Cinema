b988d240b564228001eb1b0b6868e86b936c3c17
Chen, Rick (Agoda) <rick.chen@agoda.com>
2019-03-21 02:46:48 +0700

    Add back Assets and create discover cell view model

---
+++ Cinema/App/UIComponent/DiscoverCellViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  DiscoverCellViewModel.swift
+//  Cinema
+//
+//  Created by Chen, Rick (Agoda) on 21/3/2562 BE.
+//  Copyright © 2562 Chen, Rick (Agoda). All rights reserved.
+//
+
+import Foundation

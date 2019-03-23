87ee500b0fcdf9c8b68469f5440b77d61e06eb1f
Chen, Rick (Agoda) <rick.chen@agoda.com>
2019-03-23 21:27:23 +0700

    Remove unnecessary files

--- Cinema/Core/Model/ModelsProtocol.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ModelsProtocol.swift
-//  Cinema
-//
-//  Created by Chen, Rick (Agoda) on 23/3/2562 BE.
-//  Copyright © 2562 Chen, Rick (Agoda). All rights reserved.
-//
-
-import Foundation

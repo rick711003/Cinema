393aebcd89ecd2c985ebc9bed8a231c98cf4957e
Chen, Rick (Agoda) <rick.chen@agoda.com>
2019-03-23 14:23:59 +0700

    Move folder structure

---
+++ CinemaTests/Core/TMDBServiceTests.swift
@@ -0,0 +1,29 @@
+//
+//  TMDBServiceTests.swift
+//  CinemaTests
+//
+//  Created by Chen, Rick (Agoda) on 23/3/2562 BE.
+//  Copyright © 2562 Chen, Rick (Agoda). All rights reserved.
+//
+
+import Alamofire
+import XCTest
+
+final class TMDBServiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testGetDiscover() {
+        
+    }
+    
+    func testGetMovie() {
+        
+    }
+}

697f346df3e71abff92b7a42258eda50bb625f1f
Chen, Rick (Agoda) <rick.chen@agoda.com>
2019-03-23 21:21:36 +0700

    Create unit test files

---
+++ CinemaTests/App/Discover/DiscoverRouterTests.swift
@@ -0,0 +1,9 @@
+//
+//  DiscoverRouterTests.swift
+//  CinemaTests
+//
+//  Created by Chen, Rick (Agoda) on 23/3/2562 BE.
+//  Copyright © 2562 Chen, Rick (Agoda). All rights reserved.
+//
+
+import Foundation
---
+++ CinemaTests/App/Movie/MovieBuilderTests.swift
@@ -0,0 +1,9 @@
+//
+//  MovieBuilderTests.swift
+//  CinemaTests
+//
+//  Created by Chen, Rick (Agoda) on 23/3/2562 BE.
+//  Copyright © 2562 Chen, Rick (Agoda). All rights reserved.
+//
+
+import Foundation
